13c45898aa42459e39bcbb9d04bee2d5d0d6cdb9
Sahar Sami <sahar.sami115@gmail.com>
2021-01-05 10:46:52 -0500

    different build

--- _next/static/chunks/pages/index-78abb94fc2cb6603ada2.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[16],{UchZ:function(n,_,o){"use strict";o.r(_),o.d(_,"__N_SSG",(function(){return w}));var u=o("JpYW"),w=!0;_.default=u.a},vlRD:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return o("UchZ")}])}},[["vlRD",0,1,9,7,8,5,6,2,3,4]]]);
